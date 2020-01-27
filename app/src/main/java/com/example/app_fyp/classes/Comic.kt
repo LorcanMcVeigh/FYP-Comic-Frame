05ee3d91e1ed6c9fe655cdedcbca3b561f10f9d3
LorcanMcVeigh <lorcan.mcveigh@gmail.com>
2020-01-27 21:42:42 +0000

    Fixed bugs in transfering data between activities
    *Rounded out corners on buttons and edittext boxes

--- app/src/main/java/com/example/app_fyp/classes/Comic.kt
+++ app/src/main/java/com/example/app_fyp/classes/Comic.kt
@@ -2,5 +2,5 @@ package com.example.app_fyp.classes
 
 import java.io.Serializable
 
-class Comic(val name : String, var image : String?, val issue : ArrayList<Int>?, val artist : String?) : Serializable {
+class Comic(var name : String, var image : String?, var issue : ArrayList<Int>?, val artist : String?, val hash : Int) : Serializable {
 }
